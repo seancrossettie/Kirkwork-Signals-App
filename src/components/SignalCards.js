e2184de967dbb4e7d540189d374ce5bc06f461b6
Sean Rossettie <seancrossettie@gmail.com>
2021-10-13 18:06:17 -0500

    Printing cards onto pages

--- src/components/SignalCards.js
+++ src/components/SignalCards.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SignalCards = ({ id }) => (
+const SignalCards = ({
+  alarmNum, eventCodeDesc, pointDesc, signalCode, xmit, siteDate
+}) => (
     <div>
       <h1>
-        {id}
+        {alarmNum}
+        {eventCodeDesc}
+        {pointDesc}
+        {signalCode}
+        {xmit}
+        {siteDate}
       </h1>
     </div>
 );
 
 SignalCards.propTypes = {
-  id: PropTypes.string
+  alarmNum: PropTypes.any,
+  eventCodeDesc: PropTypes.string,
+  pointDesc: PropTypes.string,
+  signalCode: PropTypes.string,
+  xmit: PropTypes.string,
+  siteDate: PropTypes.string
 };
 
 export default SignalCards;
